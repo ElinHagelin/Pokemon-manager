80ba36f0148b8d1d0bb4f25258e4d63b50fe0ba0
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-02-21 12:04:27 +0100

    Påbörjat stylingen

---
+++ script/script.js
@@ -0,0 +1,3 @@
+const searchButton = document.querySelector('.button--search')
+const teamButton = document.querySelector('.button--team')
+const mainContent = document.querySelector('.main__content')
---
+++ script/utils.js
@@ -0,0 +1,8 @@
+
+
+function createSearchView() {
+	const searchLabel = document.createElement('label')
+	const searchInput = document.createElement('input')
+	searchInput.setAttribute('id', 'search-input')
+	searchLabel.htmlFor = 'search-input'
+}
